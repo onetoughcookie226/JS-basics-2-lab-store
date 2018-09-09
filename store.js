4667c01895530bdfad5f0154c6c7f8e8a3a3d6dd
hezus <ben@leipeshit.com>
2018-09-09 22:30:28 +0200

    basics working

--- store.js
+++ store.js
@@ -1,30 +1,62 @@
-// TODO:
-// var products = []
-var products = []
-// Declare `shoppingCart`, something where you will be storing all products that the user buys.
-// Declare `products`, the different that you will be selling under each of the departments.
 
 var shopFromStore =  function () {
   var refNr = askUserForReferenceNumber();
   // Add the product with the matching referenceNumber to the shoppingCart
-
-  displayProductsFromShoppingCart();
-
+  var product = products.find(p => p.referenceNumber === refNr)
+  if(product){
+    shoppingCart.push(product);
+    addProductsFromShoppingCart(product);
+  }
   // calculate the total price of your cart, and use it:
   displayTotalPrice(/*The variable holding the totol price*/);
 
   // Ask the user if they want to continue shopping,
   // if yes, call this function again
+  if(window.confirm('Would you like to buy another item?')){
+    setTimeout(shopFromStore);
+  }else{
+    window.alert('thank you for shopping')
+  }
   // if no, print the goodbye message
 };
 
-var displayProductsFromShoppingCart = function() {
+var addProductsFromShoppingCart = function(product) {
   // iterate over the shoppingCart and display the contents
+  
 
+    // create elements for refNr, name, price, with a class and the proper innerText
+    var referenceNumberElement = document.createElement('span');
+    referenceNumberElement.className  = 'referenceNumber';
+    referenceNumberElement.innerText = product.referenceNumber;
+    // referenceNumberElement.onclick = function () {
+    //   var productNumber = this.innerHTML;
+
+    // };
+
+    var nameElement = document.createElement('span');
+    nameElement.className  = 'name';
+    nameElement.innerText = product.name;
+
+    var priceElement = document.createElement('span');
+    priceElement.className  = 'price';
+    priceElement.innerText = product.price;
+
+    // Wrap our just created elements in a div
+    var productElement = document.createElement('div');
+    productElement.className = 'product';
+
+    productElement.appendChild(referenceNumberElement);
+    productElement.appendChild(nameElement);
+    productElement.appendChild(priceElement);
+
+    // Hang that div on the page
+    document.getElementById('shopping-cart').appendChild(productElement);
+  
   // use the printProductsOnScreen function for inspiration
 };
 
 var askUserForReferenceNumber = function() {
+  return parseInt(window.prompt("Which item would you like to add to your chart?"));
   // Use window.prompt to ask the user a question and capture their response,
   // then, return the response from this function back to our caller
 };
@@ -44,6 +76,10 @@ var printProductsOnScreen = function () {
     var referenceNumberElement = document.createElement('span');
     referenceNumberElement.className  = 'referenceNumber';
     referenceNumberElement.innerText = product.referenceNumber;
+    // referenceNumberElement.onclick = function () {
+    //   var productNumber = this.innerHTML;
+
+    // };
 
     var nameElement = document.createElement('span');
     nameElement.className  = 'name';
@@ -63,15 +99,15 @@ var printProductsOnScreen = function () {
 
     // Hang that div on the page
     document.getElementById('product-overview').appendChild(productElement);
-
-
   }
 };
 
 var runApp = function () {
   printProductsOnScreen();
-
-  shopFromStore();
+  
+  setTimeout(shopFromStore);
+  
+  
 };
 
 document.onreadystatechange = function () {
@@ -79,3 +115,15 @@ document.onreadystatechange = function () {
     runApp();
   }
 };
+
+var products = [
+  { referenceNumber: 1000, name: "React", price: 10 },
+  { referenceNumber: 1001, name: "Express", price: 20 },
+  { referenceNumber: 1002, name: "Vue.js", price: 30 },
+  { referenceNumber: 1003, name: "React Native", price: 30 },
+  { referenceNumber: 1004, name: "AngularJS", price: 200 },
+  { referenceNumber: 1005, name: "Electron", price: 300 },
+  { referenceNumber: 1006, name: "Cordova", price: 30 },
+  { referenceNumber: 1007, name: "Backbone", price: 10 }
+];
+var shoppingCart = [];
